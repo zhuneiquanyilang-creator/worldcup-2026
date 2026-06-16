b685c4a35e0a143a23279cb15ab547a14da32943
take <zhuneiquanyilang@gmail.com>
2026-06-17 05:32:49 +0900

    マッチ49更新

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -8,7 +8,7 @@ export function Header() {
       <div className={styles.inner}>
         <Link to="/" className={styles.brand}>
           <span className={styles.logo}>FIFA</span>
-          <span className={styles.title}>World Cup 2026</span>
+          <span className={styles.title}>WORLD CUP 2026</span>
         </Link>
         <TeamSearch />
         <span className={styles.host}>USA · Canada · Mexico</span>
